20caa82e13396f034fd876ed8af84a5493936cb1
jantimpe <timpe31@gmail.com>
2018-04-28 16:07:03 -0500

    added video window

--- client/script2.py
+++ client/script2.py
@@ -7,7 +7,10 @@ camera = cv2.VideoCapture(0)
 scanner = zbar.ImageScanner()
 scanner.parse_config('enable')
 
+items = []
+
 if __name__ == '__main__':
+    print 'go'
     while True:
         (grabbed, frame) = camera.read()
 
@@ -25,5 +28,15 @@ if __name__ == '__main__':
         for symbol in image:
             # do something useful with results
             print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+            if not symbol.data in items:
+                items.append(symbol.data)
+
+        # show the frame and record if the user presses a key
+        cv2.imshow("Barcode Detection", frame)
+            
+        # if the 'q' key is pressed, stop the loop
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
 
         time.sleep(1)
