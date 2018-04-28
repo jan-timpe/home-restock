566d930a33c57924b5ea86e628218b40a3b78383
jantimpe <timpe31@gmail.com>
2018-04-28 17:28:56 -0500

    added weight "sensors"

--- client/script2.py
+++ client/script2.py
@@ -17,6 +17,33 @@ def update_server_state(item, weight):
         'weight': weight
     })
 
+def update_empty_items_server_state(item):
+    url = 'http://localhost:5000/update/' + str(item) + '/'
+    r = requests.post(url, data={
+        'empty': True
+    })
+
+
+
+
+number_of_times_counted = {}
+def get_updated_item_weight(item, weight):
+    if not item in number_of_times_counted:
+        number_of_times_counted[item] = 0
+    else if number_of_times_counted[item] % 5 == 0:
+        new = weight - weight*.2
+
+        if new <= 0:
+            new = 0
+            update_empty_items_server_state(item)
+
+        return new
+
+    return weight
+
+
+
+
 if __name__ == '__main__':
     print 'go'
     while True:
@@ -42,6 +69,13 @@ if __name__ == '__main__':
                 items.append(barcode)
                 item_weights[barcode] = 200
                 update_server_state(barcode, item_weights[barcode])
+            else:
+                new_weight = get_updated_item_weight(barcode, item_weights[barcode])
+
+                if new_weight != item_weights[barcode]:
+                    item_weights[barcode] = new_weight
+                    update_server_state(barcode, item_weights[barcode])
+
 
         # show the frame and record if the user presses a key
         cv2.imshow("Barcode Detection", frame)
@@ -51,4 +85,4 @@ if __name__ == '__main__':
         if key == ord("q"):
             break
 
-        time.sleep(1)
+        # time.sleep(1)
