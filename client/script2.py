5dc02d9aeabdf7740e2c6e35e7776ed9cb13c148
jantimpe <timpe31@gmail.com>
2018-04-28 16:20:21 -0500

    added server

--- client/script2.py
+++ client/script2.py
@@ -9,6 +9,16 @@ scanner.parse_config('enable')
 
 items = []
 
+item_weights = {}
+
+
+def update_server_state(items, item_weights):
+    for item in items:
+        url = 'http://localhost:5000/update/' + str(item) + '/'
+        r = requests.post(url, data={
+            'weight': item_weights[item]
+        })
+
 if __name__ == '__main__':
     print 'go'
     while True:
@@ -30,6 +40,11 @@ if __name__ == '__main__':
             print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
             if not symbol.data in items:
                 items.append(symbol.data)
+                item_weights[symbol.data] = 200
+
+        if len(image) > 0:
+            update_server_state(items, item_weights)
+            time.sleep(10)
 
         # show the frame and record if the user presses a key
         cv2.imshow("Barcode Detection", frame)
